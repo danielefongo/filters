dc32babb7d584df0a6e492f13b2aafcc82b39ca5
danielefongo <danielefongo@gmail.com>
2020-05-26 13:48:06 +0200

    add Ne condition

---
+++ test/condition/ne_test.exs
@@ -0,0 +1,31 @@
+defmodule Filters.Condition.NeTest do
+  use ExUnit.Case
+  use DbTest
+  alias Filters.Condition.Ne
+  require Logger
+
+  test "retrieve zero elements" do
+    insert([%Sample{name: "foo", surname: "bar"}, %Sample{name: "foo", surname: "bar2"}])
+
+    results = find_with_condition(by(%{[Ne, :all] => [name: "foo"]}))
+
+    assert results == []
+  end
+
+  test "retrieve one element" do
+    insert([%Sample{name: "foo", surname: "bar"}, %Sample{name: "foo2", surname: "bar2"}])
+
+    [retrieved_sample | []] = find_with_condition(by(%{[Ne, :all] => [name: "foo", surname: "bar"]}))
+
+    assert retrieved_sample.name == "foo2"
+  end
+
+  test "retrieve two elements" do
+    insert([%Sample{name: "foo", surname: "bar"}, %Sample{name: "foo", surname: "bar2"}])
+
+    [first, second | []] = find_with_condition(by(%{[Ne, :all] => [surname: "unknown_surname"]}))
+
+    assert first.surname == "bar"
+    assert second.surname == "bar2"
+  end
+end
