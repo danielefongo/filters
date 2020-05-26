6452c0954ef9b2e15b8d3ae6161763b20aacf947
danielefongo <danielefongo@gmail.com>
2020-05-26 13:16:01 +0200

    add behaviour for Condition

--- lib/filters/condition/generic.ex
+++ lib/filters/condition/generic.ex
@@ -2,8 +2,13 @@ defmodule Filters.Condition.Generic do
   defmacro __using__(_) do
     quote do
       import Ecto.Query
+      @behaviour Filter.ConditionBehaviour
 
       def run(kind, data), do: apply(Filters, kind, [data, &filter_callback/1])
     end
   end
+end
+
+defmodule Filter.ConditionBehaviour do
+  @callback filter_callback(term()) :: boolean() | %Ecto.Query.DynamicExpr{}
 end
